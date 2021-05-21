c33cbe7cf228fb8201abc44b3681444316684e3c
ginabomination <grugerstorz@gmail.com>
2021-05-21 19:49:15 +1200

    p2

--- views/dropdown.js
+++ views/dropdown.js
@@ -1,9 +1,10 @@
 const Succulents = document.querySelector("#s");
 const sbuttton = document.querySelector("#sbutton");
-
+const pump = document.getElementsByClassName("pump")
+const soil = document.querySelector("#soil")
 const arry = ["10", "20", 5, "40"];
 
 sbutton.onclick = function sresponse() {
-  console.log(pump.value = quoteArray[2]);
+  method.POST(pump.value = quoteArray[2]);
   console.log(soil.value = quoteArray[2]);
   } 
