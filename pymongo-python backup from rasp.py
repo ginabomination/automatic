6cb5f2412610755886b9372277d422ab2a701e9b
ginabomination <64622072+ginabomination@users.noreply.github.com>
2021-05-24 09:34:00 +1200

    print if connection successful

--- pymongo-python backup from rasp.py
+++ pymongo-python backup from rasp.py
@@ -1,2 +1,4 @@
 from pymongo import MongoClient
-client = MongoClient('mongob=srv://ginags:2tripleX@database.bt4bd.mongodb.net/test);
+client = MongoClient('mongob=srv://ginags:2tripleX@database.bt4bd.mongodb.net/test)
+print("Connection Successful")
+client.close()
