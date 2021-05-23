029585ef0b84153b5fca625e6906740c8af70a8d
ginabomination <64622072+ginabomination@users.noreply.github.com>
2021-05-24 09:45:56 +1200

    fixing syntax error

--- pymongo-python backup from rasp.py
+++ pymongo-python backup from rasp.py
@@ -1,4 +1,4 @@
 from pymongo import MongoClient
-client = MongoClient('mongob=srv://ginags:2tripleX@database.bt4bd.mongodb.net/test)
+client = MongoClient('mongodb=srv://ginags:2tripleX@database.bt4bd.mongodb.net/test)
 print("Connection Successful")
 client.close()
