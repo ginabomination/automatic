6fe2b2bd6cd39c21a70b03442b6ba7829d318f89
ginabomination <grugerstorz@gmail.com>
2021-06-01 13:11:27 +1200

    document found test

--- python.py
+++ python.py
@@ -3,3 +3,6 @@ client = pymongo.MongoClient('mongodb+srv://ginags:2tripleX@database.bt4bd.mongo
 dblist = client.list_database_names()
 if "Sustain" in dblist:
   print("Sustain exists.")
+
+db = client["Sustain"]
+print(db.list_collection_names())
