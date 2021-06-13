36bc7a955a9683c8c0cf028e5585246422b0d89b
ginabomination <grugerstorz@gmail.com>
2021-06-14 10:49:52 +1200

    'Moisture not defined' err

--- server.js
+++ server.js
@@ -16,16 +16,17 @@ app.listen(3000, function() {
   useUnifiedTopology: true
 }, (erro, client) => {
     const db = client.db('Sustain')
-    const Collection = db.collection('Moisture')
-       app.post('Moisture', (req, res) => {
+    const Collection = db.collection('/Moisture')
+       app.post('/Moisture', (req, res) => {
         Collection.updateOne(req.body)
           .then(result => {
             console.log(result)
-            res.redirect()
-            res.render('index.ejs')
+            res.redirect('/Moisture')
           })
           .catch(error => console.error(error))
-      })
-  if (erro) return console.error(erro)
+          if (erro) return console.error(erro)
   console.log('Connected to Database')
-})
+      })
+      app.get('/', (req, res) => {
+        res.render('index.ejs')
+      })})
