26b4d05cdc2f0092f12e8aac6aa4077d3860cc93
ginabomination <grugerstorz@gmail.com>
2021-06-24 10:08:21 +1200

    create working

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.listen(3000, function() {
   })
   
   app.post('/Moisture', (req, res) => {
-        Collection.updateOne(req.body)
+        Collection.insertOne(req.body)
           .then(result => {
             console.log(result)
             res.redirect('/')
