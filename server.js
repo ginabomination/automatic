1c109f4891b112b0efc9407980bd9875b7454157
ginabomination <grugerstorz@gmail.com>
2021-07-02 15:01:23 +1200

    working on new update code

--- server.js
+++ server.js
@@ -10,54 +10,12 @@ app.listen(3000, function() {
   })
        
 
-  MongoClient.connect('mongodb+srv://ginags:2tripleX@database.bt4bd.mongodb.net/test', {
-  useUnifiedTopology: true
-}, (err, client) => {
-  const db = client.db('Sustain')
-  const Collection = db.collection('Moisture') 
-  
-  app.get('/', (req, res) => {
-    db.collection('Moisture').find().toArray()
-    .then(results => { 
-      res.render('index.ejs', results)
-})
-.catch(error => console.error(error))
-  })
-  
-  app.post('/Moisture', (req, res) => {
-    if (err) throw err;
-  
-   
-    .then(result => {
-    console.log(result)
-    res.redirect('/')
-  })
-  .catch(error => console.error(error))
-})
-if (err) return console.error(err)
-console.log('Connected to Database')
-console.log(res.result.nModified + " document updated")
-})
-  
-
-const express = require('express');
-const bodyParser= require('body-parser');
-const app = express();
-const MongoClient = require('mongodb').MongoClient
-
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({ extended: true }))
-app.listen(3000, function() {
-    console.log('listening on 3000')
-  })
-       
-
-  MongoClient.connect('mongodb+srv://ginags:2tripleX@database.bt4bd.mongodb.net/test', {
+MongoClient.connect('mongodb+srv://ginags:2tripleX@database.bt4bd.mongodb.net/test', {
   useUnifiedTopology: true
 }, (err, client) => {
   const db = client.db('Sustain') 
   
-  app.get('/', (req, res) => {
+  app.get('/', (res) => {
     db.collection('Moisture').find().toArray()
     .then(results => { 
       res.render('index.ejs', results)
@@ -65,12 +23,13 @@ app.listen(3000, function() {
 .catch(error => console.error(error))
   })
   
-  app.post('/Moisture', (req, res) => {
+  app.post('/Moisture', (res) => {
     var dbo = db.db("Sustain");
   var myquery = { sort: "1" };
   var newvalues = { $set: { pump: "3", soil: "7"}};
   const Collection = db.collection("Moisture");
   Collection.updateMany(myquery, newvalues)
+  console.log(res.result.nModified + " document updated")
 
           .then(result => {
             console.log(result)
@@ -80,5 +39,4 @@ app.listen(3000, function() {
       })
       if (err) return console.error(err)
   console.log('Connected to Database')
-  console.log(res.result.nModified + " document updated")
 })
