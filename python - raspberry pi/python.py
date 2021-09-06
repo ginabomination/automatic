4adf45d9c10e52e8f17c3a618805c70e58d2a2ed
ginabomination <grugerstorz@gmail.com>
2021-09-07 11:09:01 +1200

    "

--- python - raspberry pi/python.py
+++ python - raspberry pi/python.py
@@ -4,13 +4,12 @@ dblist = client.list_database_names()
 db = client["Sustain"]
 collist = db.list_collection_names()
 col = db["Moisture"]
+
+cur = col.find()  
+for doc in cur:
+    print(doc)
+
 query = { 'pump': "" };
 x = db.col.find(query)
 print(query)
 print(x)
-
-cur = col.find()  
-cur (x)
-for doc in cur:
-    print(doc)
-
