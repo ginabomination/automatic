5c6d9c95372d37200d0c337d104fe0ba65c36b28
ginabomination <grugerstorz@gmail.com>
2021-09-17 01:04:56 +1200

    combining soil mositure code with database pull code

--- python - raspberry pi/python.py
+++ python - raspberry pi/python.py
@@ -1,10 +1,19 @@
 import pymongo
-from bson.json_util import dumps, loads
+import time 
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in as AnalogIn
+
 client = pymongo.MongoClient('mongodb+srv://ginags:2tripleX@database.bt4bd.mongodb.net/test')
 dblist = client.list_database_names()
 db = client["Sustain"]
 collist = db.list_collection_names()
 col = db["Moisture"]
+
+i2c = busio.I2C(board.SCL, board.SDA)
+ads = ADS.ADS1115(i2c)
+chan = AnalogIn(ads, ADS.P0)
 
 for PlaceholderPump in col.find():
   print ('pump:')
@@ -15,3 +24,13 @@ for PlaceholderSoil in col.find():
   print ('soil:')
   soil = (PlaceholderSoil['soil'])
   print (soil)
+
+print ("{:>5}\t{:>5}".format("raw", "v"))
+
+while True
+def forever():
+  if chan.voltage > soil
+    print ('*pump runs a cycle*')
+    time.sleep(18000)
+  else:
+    time.sleep(18000) #sleep for 5 hrs
