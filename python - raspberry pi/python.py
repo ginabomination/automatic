656d6f544d8f96f010d3275ba2e273545ee87480
ginabomination <grugerstorz@gmail.com>
2021-09-21 11:17:27 +1200

    combing all raspi python code

--- python - raspberry pi/python.py
+++ python - raspberry pi/python.py
@@ -1,9 +1,15 @@
 import pymongo
+
+
+import RPi.GPIO as GPIO
 import time 
+
+
 import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in as AnalogIn
+
 
 client = pymongo.MongoClient('mongodb+srv://ginags:2tripleX@database.bt4bd.mongodb.net/test')
 dblist = client.list_database_names()
@@ -11,26 +17,39 @@ db = client["Sustain"]
 collist = db.list_collection_names()
 col = db["Moisture"]
 
+
 i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c)
 chan = AnalogIn(ads, ADS.P0)
 
+
+ledpin = 24
+GPIO.setmode(GPIO.BOARD)
+print ("pin setup")
+GPIO.setup(ledpin, GPIO.OUT)
+GPIO.setwarnings(False)
+
+
 for PlaceholderPump in col.find():
-  print ('pump:')
   pump = (PlaceholderPump['pump'])
   print (pump)
 
 for PlaceholderSoil in col.find():
-  print ('soil:')
   soil = (PlaceholderSoil['soil'])
   print (soil)
+  
 
 print ("{:>5}\t{:>5}".format("raw", "v"))
 
 while True
-def forever():
-  if chan.voltage > soil
-    print ('*pump runs a cycle*')
-    time.sleep(18000)
-  else:
-    time.sleep(18000) #sleep for 5 hrs
+
+def on_forever():
+if chan.voltage > 1.600
+  GPIO.output(ledpin, True)
+  print ("pump on")
+  time.sleep(pump)
+  GPIO.output(ledpin, False)
+  print ("pump off")
+  time.sleep(18000) #sleep for 5 hrs
+else:
+  time.sleep(7200) #sleep for 2 hrs
