6341d13ff247f5056dc288e4d415ee9f9da8415b
ginabomination <64622072+ginabomination@users.noreply.github.com>
2021-10-29 11:34:58 +0100

    trouble shooting combined code on raspi - '>' not supported between instances of 'float' and 'Decimal128'

--- python - raspberry pi/python.py
+++ python - raspberry pi/python.py
@@ -1,15 +1,10 @@
 import pymongo
-
-
 import RPi.GPIO as GPIO
-import time 
-
-
+import time
 import board
 import busio
-import adafruit_ads1x15.ads1115 as ADS
-from adafruit_ads1x15.analog_in as AnalogIn
-
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 
 client = pymongo.MongoClient('mongodb+srv://ginags:2tripleX@database.bt4bd.mongodb.net/test')
 dblist = client.list_database_names()
@@ -17,39 +12,37 @@ db = client["Sustain"]
 collist = db.list_collection_names()
 col = db["Moisture"]
 
-
 i2c = busio.I2C(board.SCL, board.SDA)
-ads = ADS.ADS1115(i2c)
+ads = ADS.ADS1015(i2c)
 chan = AnalogIn(ads, ADS.P0)
-
-
 ledpin = 24
-GPIO.setmode(GPIO.BOARD)
+
 print ("pin setup")
 GPIO.setup(ledpin, GPIO.OUT)
 GPIO.setwarnings(False)
 
-
 for PlaceholderPump in col.find():
-  pump = (PlaceholderPump['pump'])
-  print (pump)
-
+    pump = (PlaceholderPump['pump'])
+    print (pump)
+    
 for PlaceholderSoil in col.find():
-  soil = (PlaceholderSoil['soil'])
-  print (soil)
-  
+    soil = (PlaceholderPump['soil'])
+    print (soil)
+    
+print ("{:>5}\t{:>5}".format ("raw", "v"))
 
-print ("{:>5}\t{:>5}".format("raw", "v"))
+f = float(soil)
+print ('Float Value =', f)
 
-while True
+print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
 
-def on_forever():
-if chan.voltage > 1.600
-  GPIO.output(ledpin, True)
-  print ("pump on")
-  time.sleep(pump)
-  GPIO.output(ledpin, False)
-  print ("pump off")
-  time.sleep(18000) #sleep for 5 hrs
+while True:
+    if chan.voltage > soil:
+        GPIO.output(ledpin, GPIO.HIGH)
+        print ("pump on")
+        time.sleep(pump)
+        GPIO.output(ledpin, GPIO.LOW)
+        print ("pump off")
+        time.sleep(18000)
 else:
-  time.sleep(7200) #sleep for 2 hrs
+    time.sleep(7200)
